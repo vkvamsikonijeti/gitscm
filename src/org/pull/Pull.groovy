f4a51de018a914c045502926ce794f59ddeb7ec3
vkvamsikonijeti <vkvamsikonijeti@gmail.com>
2018-02-02 20:55:54 +0530

    Added Groovy File

--- src/org/pull/Pull.groovy
+++ src/org/pull/Pull.groovy
@@ -3,3 +3,15 @@ package org.pull
 class Pull{
   def config
   def script
+  
+  Pull(script,config){
+    this.config=config
+    this.script=script
+  }
+  void pullcode(){
+    this.scrpit.stage('pulling'){
+    this.script.sh('git clone this.config.url')
+  }
+ }
+}
+    
